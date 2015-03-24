dc0b3598322ed2359aea012bfa421c7410350901
dpouchon <david.pouchon@batiactugroupe.com>
2015-03-24 16:34:25 +0100

    bb

--- js/employeelist.js
+++ js/employeelist.js
@@ -10,16 +10,30 @@ $('#employeeListPage').bind('pageinit', function(event) {
 });
 
 function getEmployeeList() {
-	$.getJSON(serviceURL + 'getemployees.php', function(data) {
-		$('#employeeList li').remove();
-		employees = data.items;
-		$.each(employees, function(index, employee) {
-			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
-					'<img src="' + mediaURL + 'pics/' + employee.picture + '"/>' +
-					'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
-					'<p>' + employee.title + '</p>' +
-					'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
-		});
-		$('#employeeList').listview('refresh');
+
+
+	$.ajax({
+		url: serviceURL + 'getemployees.php',
+		//data: {json:1, appli:appliName, ver: appliVersion, put:'etat_push', token:token, uuid:device_uuid, info:info, data: data},
+		dataType: 'json',
+		crossDomain: true,
+		type: "POST",
+		success: function(data) {
+			console.log(data);
+			$('#employeeList li').remove();
+
+			console.log(data.items);
+
+			employees = data.items;
+			$.each(employees, function(index, employee) {
+				$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+				'<img src="' + mediaURL + 'pics/' + employee.picture + '"/>' +
+				'<h4>' + employee.firstName + ' ' + employee.lastName + '</h4>' +
+				'<p>' + employee.title + '</p>' +
+				'<span class="ui-li-count">' + employee.reportCount + '</span></a></li>');
+			});
+			$('#employeeList').listview('refresh');
+		}
 	});
+
 }
