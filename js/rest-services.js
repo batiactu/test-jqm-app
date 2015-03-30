6b7fabc30d66c546d3ea88f1bbb11e160abe4442
dpouchon <david.pouchon@batiactugroupe.com>
2015-03-30 08:55:18 +0200

    modif

--- js/rest-services.js
+++ js/rest-services.js
@@ -3,10 +3,10 @@
 angular.module('myApp.restServices', ['ngResource'])
     .factory('Employee', ['$resource',
         function ($resource) {
-            return $resource('http://localhost:3000/employees/:employeeId', {});
+            return $resource('http://recherche.batiactu.com/test_mobile/version1employes/services/emplyee.php?id=:employeeId', {});
         }])
 
     .factory('Report', ['$resource',
         function ($resource) {
-            return $resource('http://localhost:3000/employees/:employeeId/reports', {});
+            return $resource('http://recherche.batiactu.com/test_mobile/version1employes/services/emplyee.php?id=:employeeId/reports', {});
         }]);
